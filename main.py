d2160dbf921ebe0012b8915f33cc984cf997a0f9
M.Luperto <78561125+Lupi91@users.noreply.github.com>
2024-04-04 09:55:07 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-from pathlib import Path
-import sys
+import threading
 import os
-from time import perf_counter  
+from time import perf_counter
 
+import customtkinter as ctk
+from customtkinter import StringVar
+import tkinter as tk
+from CTkListbox import *
 from faster_whisper import WhisperModel
 import ctranslate2
 import sentencepiece as spm
-os.environ["CT2_VERBOSE"] = "-1"    # -1 = error https://opennmt.net/CTranslate2/environment_variables.html
-
+#os.environ["CT2_VERBOSE"] = "-1"    # -1 = error https://opennmt.net/CTranslate2/environment_variables.html
 ct_model_path = "models/NLLB/nllb-200-distilled-600M-int8"
 sp_model_path = "models/NLLB/flores200_sacrebleu_tokenizer_spm.model"
 
 
-def translate2(source_sents, sp, translator):
-    source_sentences = [source_sents]
-
+def translate2(source_sents, sp, translator, tgt_lang, beam_size):
     src_lang = "eng_Latn"
-    tgt_lang = "deu_Latn" # default output is German
-    # List of language codes, see: https://github.com/facebookresearch/flores/blob/main/flores200/README.md#languages-in-flores-200
-
-    beam_size = 5
+    source_sentences = [source_sents]
 
     source_sentences = [sent.strip() for sent in source_sentences]
     target_prefix = [[tgt_lang]] * len(source_sentences)
@@ -41,113 +38,230 @@ def translate2(source_sents, sp, translator):
 
 
 
-def main(model, input, output, task):
-    model_dir = "models/whisper_medium"
-    if model == "large":
-        model_dir = "models/whisper_large"
-    print("Loading whisper model...")
-    try:
-        model = WhisperModel(model_dir, device="cuda")
-        print("Whisper ready - CUDA enabled!")
-    except:
-        try:
-            model = WhisperModel(model_dir, device="cpu")
-            print("Whisper ready - CPU enabled!")
-        except Exception as e:
-            print(e)
-            sys.exit(1)
-    
-    if task == "translate":
-        try:
-            print("Loading sentencepiece model...")
-            sp = spm.SentencePieceProcessor()
-            sp.load(sp_model_path)
-        except Exception as e:
-            print(e)
-            sys.exit(1)
+class App(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+
+        with open("config/lang_list.txt", encoding="utf-8") as f:
+            self.language_list = [line.rstrip() for line in f]
+        with open("config/lang_code.txt", encoding="utf-8") as f:
+            self.language_code = [line.rstrip() for line in f]
+        with open("config/init_lang.txt", encoding="utf-8") as f:
+            self.language_init = f.readline().strip('\n')
+
+        self.geometry("800x800")
+        self.title("Faster-Whisper-Translator")
+        self.resizable(False,False)
+        ctk.set_default_color_theme("dark-blue")
+        self.grid_rowconfigure(7, weight=1)
+        self.grid_columnconfigure((0, 1, 2, 3), weight=1)
+
+
+        self.audiobutton = ctk.CTkButton(self, command=self.select_audio, height=50, text="Select File", font=ctk.CTkFont(family="Verdana", size=16,weight="bold"))
+        self.audiobutton.grid(row=0, columnspan=4, padx=20, pady=10, sticky="nsew")
+        self.audiofilename = ""
+
+        # COL 0/1 (whisper settings)
+        self.label1 = ctk.CTkLabel(self, text="model size", font=ctk.CTkFont(weight="bold"), anchor="w", justify="left")
+        self.label1.grid(row=2, column=0, padx=20, pady=10, sticky="nw")
+
+        self.modelsize = ctk.CTkComboBox(self, values=["small", "medium", "large"], state="readonly", variable=ctk.StringVar(value="medium"))
+        self.modelsize.grid(row=2, column=1, padx=20, pady=10, sticky="nw")
+
+        self.label2 = ctk.CTkLabel(self, text="beam_size", font=ctk.CTkFont(weight="bold"), anchor="w", justify="left")
+        self.label2.grid(row=3, column=0, padx=20, pady=10, sticky="nw")
+        beam = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        self.beamsize = ctk.CTkComboBox(self, values=beam, state="readonly", variable=ctk.StringVar(value=5))
+        self.beamsize.grid(row=3, column=1, padx=20, pady=10, sticky="nw")
+
+        self.label3 = ctk.CTkLabel(self, text="device", font=ctk.CTkFont(weight="bold"), anchor="w", justify="left")
+        self.label3.grid(row=4, column=0, padx=20, pady=10, sticky="nw")
+        self.device = ctk.CTkComboBox(self, values=["auto", "cpu", "cuda"], state="readonly", variable=ctk.StringVar(value="auto"))
+        self.device.grid(row=4, column=1, padx=20, pady=10, sticky="nw")
+
+        self.label4 = ctk.CTkLabel(self, text="VAD filter", font=ctk.CTkFont(weight="bold"), anchor="w", justify="left")
+        self.label4.grid(row=5, column=0, padx=20, pady=10, sticky="nw")
+        self.vad_filter = ctk.CTkComboBox(self, values=["True", "False"], state="readonly", variable=ctk.StringVar(value="False"))
+        self.vad_filter.grid(row=5, column=1, padx=20, pady=10, sticky="nw")
+
+
+        # COL 2/3 (task/language)
+        self.rb_task_var = StringVar()
+        self.rb3 = ctk.CTkRadioButton(self, text="transcribe", variable=self.rb_task_var, value="transcribe", command=self.radiobutton_event)
+        self.rb4 = ctk.CTkRadioButton(self, text="translate", variable=self.rb_task_var, value="translate", command=self.radiobutton_event)
+        self.rb3.grid(row=2, column=2, columnspan=1, padx=20, pady=10, sticky="nw")
+        self.rb4.grid(row=2, column=3, columnspan=1, padx=20, pady=10, sticky="nw")
+        self.rb3.select()
+
+        self.label5 = ctk.CTkLabel(self, text="Target Language", width=140, font=ctk.CTkFont(weight="bold"), anchor="w", justify="left")
+        self.label5.grid(row=3, column=2, padx=20, pady=10, sticky="nw")
+        self.tb1 = ctk.CTkEntry(self)
+        self.tb1.grid(row=4, column=2, padx=20, pady=10, sticky="nw")
         
+        self.listbox = CTkListbox(self, width=140, font=ctk.CTkFont(size=10))
+        self.listbox.grid(row=3, column=3, rowspan=4, padx=20, pady=10, sticky="nsew")
+        for i, item in enumerate(self.language_list):
+            self.listbox.insert(i, item)
+
+        self.tb1.bind('<KeyRelease>', self.checkkey)
+        self.tb1.insert("insert", self.language_init)
+        self.checkkey('<KeyRelease>')
+
+
+        self.transcribebutton = ctk.CTkButton(self, command=self.transcribe_button, height=50, text="Run", font=ctk.CTkFont(family="Verdana", size=16,weight="bold"))
+        self.transcribebutton.grid(row=6, column=0, columnspan=2, padx=20, pady=20, sticky="nsew")
+        self.console = ctk.CTkTextbox(self)
+        self.console.grid(row=7, column=0, columnspan=4, padx=20, pady=10, sticky="nsew")
+        self.radiobutton_event()
+
+
+
+    def update(self, data):
+        self.listbox.delete("all")
+        for i, item in enumerate(data):
+            self.listbox.insert(i, item)
         try:
-            print("Loading translator model...")
-            translator = ctranslate2.Translator(ct_model_path, device="cuda")
-            print("Translator ready - CUDA enabled!")
-        except:
+            self.listbox.activate(0)
+        except: # IndexError or KeyError:
+            pass
+
+    def checkkey(self, event):
+        value = self.tb1.get()
+        if not value:
+            data = self.language_list
+        else:
+            data = []
+            for item in self.language_list:
+                if value.lower() in item.lower():
+                    data.append(item)
+        self.update(data)
+
+    def radiobutton_event(self):
+        if self.rb_task_var.get() == "transcribe":
+            self.label5.grid_forget()
+            self.tb1.grid_forget()
+            self.listbox.grid_forget()
+        else:
+            self.label5.grid(row=3, column=2, padx=20, pady=10, sticky="nw")
+            self.tb1.grid(row=4, column=2, padx=20, pady=10, sticky="nw")
+            self.listbox.grid(row=3, column=3, rowspan=4, padx=20, pady=10, sticky="nsew")
+            self.checkkey('<KeyRelease>')
+
+    def write(self,*message, end = "\n", sep = " "):
+        text = ""
+        for item in message:
+            text += "{}".format(item)
+            text += sep
+        text += end
+        self.console.insert("insert", text)
+        self.console.see(tk.END) # auto scrol down
+
+    def select_audio(self):
+        self.audio = tk.filedialog.askopenfilename(title="Select File")
+        self.audiofilename = os.path.basename(self.audio)
+        if self.audiofilename != "":
+            self.audiobutton.configure(text=self.audiofilename)
+            self.write("File:", self.audio)
+
+    def transcribe_button(self):
+        threading.Thread(target=self.transcribe).start()
+
+    def transcribe(self):
+        self.transcribebutton.configure(state="disabled")
+        self.console.delete('1.0', tk.END)
+        if self.audiofilename == "":
+            self.write("ERROR: No file selected!")
+            self.transcribebutton.configure(state="normal")
+            return
+
+        if self.rb_task_var.get() == "translate":
             try:
-                translator = ctranslate2.Translator(ct_model_path, device="cpu")
-                print("Translator ready - CPU enabled!")
+                if self.listbox.curselection() is None:
+                    self.write("ERROR: Target language missing!")
+                    self.transcribebutton.configure(state="normal")
+                    return
+                lang_index = self.language_list.index(self.listbox.get())
+                tgt_lang = self.language_code[lang_index]
+            except:
+                self.write("ERROR: Target language missing!")
+                self.transcribebutton.configure(state="normal")
+                return
+            if tgt_lang == "":
+                self.write("ERROR: Target language missing!")
+                self.transcribebutton.configure(state="normal")
+                return
+
+            try:
+                sp = spm.SentencePieceProcessor()
+                sp.load(sp_model_path)
             except Exception as e:
-                print(e)
-                sys.exit(1)
+                self.write("ERROR: Failed to load sentencepiece model!")
+                self.write(e)
+                self.transcribebutton.configure(state="normal")
+                return
 
-    print("\nRunning Speech Recognition...")
-    print("")
-    t1 = perf_counter() 
-    RESULT_FILES = []
-    
-    input_files = [x for x in input]
-    
-    for idx, f in enumerate(input_files):
+            try:
+                self.write(f"Target Language: {tgt_lang}")
+                self.write("Loading translator model... ")
+                translator = ctranslate2.Translator(ct_model_path, device=self.device.get())
+            except Exception as e:
+                self.write(e)
+                self.transcribebutton.configure(state="normal")
+                return
+
+
+        model_dir = "models/whisper_" + self.modelsize.get()
+        self.write(f"Loading whisper model [{model_dir}] ... ")
         try:
-            segments, info = model.transcribe(f, beam_size=5, task=task)
+            model = WhisperModel(model_dir, device=self.device.get())
         except Exception as e:
-            print("(%s/%s) ERROR processing file: %s" % (idx+1, len(input_files), f))
-            print(e)
-            print("")
-            if idx == len(input) - 1:
-                #sys.exit(1)
-                break
-            else:
-                continue
-        print("_____________________________________________________________")
-        print(f"Processing: {f}")
-        print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+            self.write("ERROR: Loading model failed!")
+            self.write(e)
+            self.transcribebutton.configure(state="normal")
+            return
 
+
+        self.write("\nRunning Speech Recognition...")
+        t1 = perf_counter()
         RESULT = []
         TRANSLATION = []
+        try:
+            v_filter = True if (self.vad_filter.get() == "True") else False
+            segments, info = model.transcribe(self.audio, beam_size=int(self.beamsize.get()), task=self.rb_task_var.get(), vad_filter=v_filter)
+        except Exception as e:
+            self.write("ERROR")
+            self.write(e)
+            self.transcribebutton.configure(state="normal")
+            return
+
+        self.write("_____________________________________________________________")
+        self.write(f"Processing: {self.audio}")
+        self.write("Detected language '%s' with probability %f" % (info.language, info.language_probability))
         for segment in segments:
-            if task == "translate":
-                translated_str = translate2(segment.text[1:], sp, translator)
-                print("(%s/%s) [%.2fs -> %.2fs] %s:" % (idx+1, len(input_files), segment.start, segment.end, translated_str))
+            self.write("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+            RESULT.append(segment.text[1:])
+            if self.rb_task_var.get() == "translate":
+                translated_str = translate2(segment.text[1:], sp, translator, tgt_lang, int(self.beamsize.get()))
                 TRANSLATION.append(translated_str)
-                RESULT.append(segment.text[1:])
-            else:
-                print("(%s/%s) [%.2fs -> %.2fs] %s" % (idx+1, len(input_files), segment.start, segment.end, segment.text))
-                RESULT.append(segment.text[1:])
-        
-        
-        if output:
-            outfile = os.path.join(output, Path(f).stem + ".txt")
-        else:
-            outfile = os.path.join(os.path.dirname(f), Path(f).stem + ".txt")
-        
-        with open(outfile, 'a', encoding="utf-8") as f:
+
+        output = os.path.join(os.path.dirname(self.audio), self.audiofilename + "_transcript.txt")
+        with open(output, "w", encoding='utf-8') as f:
             for ln in RESULT:
                 f.write("%s\n" % ln)
-            if task == "translate":
+
+            if self.rb_task_var.get() == "translate":
                 f.write("\n### TRANSLATION\n")
                 for ln in TRANSLATION:
                     f.write("%s\n" % ln)
 
-        RESULT_FILES.append(outfile)
-    
-    print("_____________________________________________________________")
-    t2 = perf_counter()
-    print("DONE!")
-    for i in RESULT_FILES:
-        print("Output:", i)
-    print("Elapsed time:", round((t2-t1), 2))
+            self.write("\nSaved transcript to " + output)
 
+        self.write("_____________________________________________________________")
+        t2 = perf_counter()
+        self.write("DONE! - Elapsed time:", round((t2-t1), 2), "sec")
+        self.transcribebutton.configure(state="normal")
 
-from gooey import Gooey, GooeyParser
-@Gooey(program_name="FASTER-WHISPER / CTranslate2 NLLB", required_cols=1, optional_cols=1)
-def parse_args():   
-    parser = GooeyParser(description="Speech Recognition / Translation") 
-    parser.add_argument("-i", "--input", required=True, nargs='*', help="Select audio or video file/s", widget="MultiFileChooser")
-    parser.add_argument("-o", "--output", required=False, help='output directory [default = input directory]', widget="DirChooser")
-    parser.add_argument("-m", "--model", choices=["medium", "large"], default="medium", help="choose model type")
-    parser.add_argument("-t", "--task", choices=["transcribe", "translate"], default="transcribe", help="choose task: transcribe or translate")
-    args = parser.parse_args()
-    return args
-
-if __name__ == '__main__':
-    opt = parse_args()
-    main(**vars(opt))
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
